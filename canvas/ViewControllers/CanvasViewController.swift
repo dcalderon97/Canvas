8a477dc1f3e266b6fb22dc004a32b747e58ead1d
Daniel Calderon <dcalderonmariavez@csumb.edu>
2018-03-12 12:17:58 -0700

    Adding files

---
+++ canvas/ViewControllers/CanvasViewController.swift
@@ -0,0 +1,101 @@
+//
+//  CanvasViewController.swift
+//  canvas
+//
+//  Created by Daniel Calderon on 3/12/18.
+//  Copyright © 2018 Daniel Calderon. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPanTray(_:)))
+        trayView.isUserInteractionEnabled = true
+        trayView.addGestureRecognizer(panGestureRecognizer)
+        // Do any additional setup after loading the view.
+        trayDownOffset = 160
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayDown
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayUp
+                }
+            }
+        }
+    }
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+
+        if sender.state == .began {
+            var imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(newlyCreatedFacePan(_:)))
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+        
+        }
+    }
+    @objc func newlyCreatedFacePan(_ sender: UIPanGestureRecognizer){
+        let translation = sender.translation(in: view)
+
+        if sender.state == .began {
+            newlyCreatedFace = sender.view as! UIImageView // to get the face that we panned on.
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center // so we can offset by translation later.
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
